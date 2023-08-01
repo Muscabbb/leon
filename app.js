799f0389ead3fc316f58d153c449b7dc7e9fa454
Muscab <muscabinho367@gmail.com>
2023-08-01 00:49:54 -0700

    the hole project is done!

---
+++ app.js
@@ -0,0 +1,7 @@
+document.querySelector(".links").addEventListener('click', () => {
+
+    document.querySelector(".ul").classList.toggle("remove");
+
+    document.querySelector(".span").classList.toggle("fill");
+
+})
